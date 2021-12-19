225fd2b9795fbab55c68ebd4ea33098db2d01563
avi <buchman.avi@gmail.com>
2021-12-19 11:09:43 +0200

    fixed allowed hosts and debug settings

--- filmproject/settings.py
+++ filmproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['calm-coast-66870.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
